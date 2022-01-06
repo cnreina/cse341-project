fc7b5da0467f656f99cb221a35a32d49fa83e5a7
Carlos N Reina <cnreina@gmail.com>
2022-01-06 12:34:36 -0700

    setup

--- routes/ta02.js
+++ routes/ta02.js
@@ -1,15 +1,36 @@
-//TA02 PLACEHOLDER
-// Remember, you can make more of these placeholders yourself!
 const express = require('express');
 const router = express.Router();
 
+let users = ["Carlos","Laurita", "Lucia"]
+
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
-    path: '/ta02', // For pug, EJS
+    path: 'ta02', // For pug, EJS
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
+    users : [],
+    numUsers : 0
   });
 });
+
+router.post('/add-user', (req, res, next) => {
+  users.push(req.body.username);
+  res.render("pages/ta02", {
+  title: 'Team Activity 02',
+  path: 'ta02',
+  users : users, 
+  numUsers : users.length});
+});
+
+router.post('/delete-user', (req, res, next) => {
+  users = users.filter((user) => user !== req.body.deleteName);
+
+  res.render("pages/ta02", {
+    title: 'Team Activity 02',
+    path: 'ta02',
+    users : users, 
+    numUsers : users.length});
+})
 
 module.exports = router;
