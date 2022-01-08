afc967deb9028ccce80f61e43dd66df3f18e976a
Carlos N Reina <cnreina@gmail.com>
2022-01-07 17:15:07 -0700

    stretch 2 completed

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 let users = []
+let errorsArray = []
 
 router.get('/', (req, res, next) => {
   res.render('pages/ta02', {
@@ -11,23 +12,53 @@ router.get('/', (req, res, next) => {
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
     users : users, 
-    numUsers : users.length
+    numUsers : users.length,
+    errorsArray : errorsArray,
+    errorsArrayCount : errorsArray.length
   });
 });
 
-router.post('/add-username', (req, res, next) => {
-  if (req.body.newUserName != '') {
-    users.push(req.body.newUserName);
+router.post('/addUser', (req, res, next) => {
+  errorsArray.length = 0
+  // empty request
+  if (req.body.addUser === '') {
+    errorsArray.push('ERROR: Empty request');
   };
+  // duplicated data
+  if (users.indexOf(req.body.addUser) >= 0) {
+    errorsArray.push('ERROR: User exist');
+  };
+
+  // add user
+  if (errorsArray.length > 0) {
+    console.log(errorsArray);
+  } else {
+    users.push(req.body.addUser);
+  }
+
+  // stay in page
   res.redirect('/ta02');
 });
 
-router.post('/delete-username', (req, res, next) => {
-  if (users.length > 0) {
-    if (req.body.deleteUserName != '') {
-      users = users.filter((user) => user !== req.body.deleteUserName);
-    };
+router.post('/removeUser', (req, res, next) => {
+  errorsArray.length = 0
+  // empty request
+  if (req.body.removeUser === '') {
+    errorsArray.push('ERROR: Empty request');
   };
+  // no local data
+  if (users.indexOf(req.body.removeUser) < 0) {
+    errorsArray.push('ERROR: User does not exist');
+  };
+
+  // remove user
+  if (errorsArray.length > 0) {
+    console.log(errorsArray);
+  } else {
+    users = users.filter((user) => user !== req.body.removeUser);
+  }
+
+  // stay in page
   res.redirect('/ta02');
 });
 
